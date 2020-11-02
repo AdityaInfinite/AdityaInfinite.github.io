4b72479dfb78d2ba81657bd67c5a849dcb745e36
aditya <dalvi.aditya14@gmail.com>
2020-11-02 16:44:39 +0530

    card added in index

--- getGithubInfo.js
+++ getGithubInfo.js
@@ -1,25 +1,36 @@
-getdata();
-func2();
-async function getdata() {
+buttonText();
+popoverText();
+async function buttonText() {
     const response = await fetch("https://api.github.com/users/AdityaInfinite");
     var data = await response.json();
     $("#repos").html(data.public_repos + " Repositories");
 
     const responce2 = await fetch("https://api.github.com/search/issues?q=+type:pr+user:AdityaInfinite&sort=created&order=asc");
     data2 = await responce2.json();
-    $("#prs").html(data2.total_count + " Pull Requests");
+    if (data2.total_count != undefined) {
+        $("#prs").html(data2.total_count + " Pull Requests");
+    }else{
+        $("#prs").html("2 Pull Requests");
+        console.log("api timeout");
+    }
+
 }
-async function func2() {
+async function popoverText() {
     const response = await fetch("https://api.github.com/users/AdityaInfinite/repos");
     var data = await response.json();
-
     let tab = "<ul>";
     for (let r of data) {
         tab += `<li><a href=${r.html_url} target="_blank">${r.name}</a></li>`;
     }
-    // Setting innerHTML as tab variable 
     tab += "</ul>";
-    console.log(tab);
-
     $("#repoButton").attr("data-content", tab);
+
+    const response2 = await fetch("https://api.github.com/search/issues?q=+type:pr+user:AdityaInfinite&sort=created&order=asc");
+    data = await response2.json();
+    let tab2 = "<ul>";
+    for (let r2 of data.items) {
+        tab2 += `<li><a href=${r2.html_url} target="_blank">${r2.title}</a></li>`;
+    }
+    tab2 += "</ul>";
+    $("#prButton").attr("data-content", tab2);
 }
