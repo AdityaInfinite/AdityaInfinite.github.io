a35738bc919673aa52a6413e3fb9212887bfe0b1
aditya <dalvi.aditya14@gmail.com>
2020-11-03 11:24:11 +0530

    adder loading spinner and Desmos Api

--- projects/generateCardForRepos.js
+++ projects/generateCardForRepos.js
@@ -3,10 +3,17 @@ getInfo();
 
 
 async function getInfo() {
-    let tab = "<div class = 'row'>";
     const response = await fetch("https://api.github.com/users/AdityaInfinite/repos");
     var data = await response.json();
+    if (response) {
+        document.getElementById('loading').style.display = 'none';
+        console.log("loader hidden")
+    }
+    show(data);
+}
 
+async function show(data){
+    let tab = "";
     for (let r of data) {
         if (r.name != "AdityaInfinite.github.io") {
             await fetch(`https://raw.githubusercontent.com/AdityaInfinite/${r.name}/main/description.json`)
@@ -15,7 +22,6 @@ async function getInfo() {
                 })
                 .then(function (data2) {
                     if (data2.isProject == true) {
-                        console.log(r.name);
                         img = `https://github-readme-stats.vercel.app/api/pin/?username=AdityaInfinite&theme=dark&repo=${r.name}`;
 
                         tab +=
@@ -24,13 +30,11 @@ async function getInfo() {
                             <img src="${img}" class="card-img-top" alt="repoImage"></img>
                             <div class="card-body">
                                 <h5 class="card-title">${r.name}</h5>
-                                <p class="card-text">Lorem Ipsum Dolor Sit Amet</p>
+                                <p class="card-text">${data2.description}</p>
                                 <a href="${r.html_url}" target="_blank" class="btn btn-primary">More info</a>
                             </div>
                             </div>
                         </div>`;
-
-                        console.log("final" + tab)
                     }
                 })
                 ;
@@ -43,7 +47,6 @@ async function getInfo() {
                 })
                 .then(function (data2) {
                     img = `https://github-readme-stats.vercel.app/api/pin/?username=AdityaInfinite&theme=dark&repo=${r.name}`;
-                    console.log("0" + tab);
                     tab +=
                         `<div class="col-sm-4.5" style="margin: 10px">
                     <div class="card">
@@ -55,18 +58,13 @@ async function getInfo() {
                     </div>
                     </div>
                 </div>`;
-                    console.log("1" + tab);
                 });
         }
-        console.log("2" + tab);
-        console.log("r: "+ data[r])
     }
-
-    tab += "</div>";
-    console.log("3" + tab);
     $("#projectsDiv").html(tab);
     console.log("end");
 }
+
 
 
 
