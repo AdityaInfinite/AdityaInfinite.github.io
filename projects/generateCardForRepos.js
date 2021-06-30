89d0cbd96b38ca64bb017836642ae4802166bea3
Aditya <dalvi.aditya14@gmail.com>
2021-06-30 19:19:09 +0530

    projects updated

--- projects/generateCardForRepos.js
+++ projects/generateCardForRepos.js
@@ -1,39 +1,26 @@
 console.log("start");
-getInfo();
-
-
-async function getInfo() {
-    const response = await fetch("https://api.github.com/users/AdityaInfinite/repos");
-    var data = await response.json();
-    if (response) {
-        document.getElementById('loading').style.display = 'none';
-        console.log("loader hidden")
-    }
-    show(data);
-}
+show()
+data = ''
 async function show(data) {
-    let tab = "";
-    for (let r of data) {
-        const response2 = await fetch(`https://raw.githubusercontent.com/AdityaInfinite/${r.name}/${r.default_branch}/description.json`);
-        console.log(response2.status);
-        if (response2.status != 404) {
-            var data2 = await response2.json();
-            if (data2.isProject == true) {
-                img = `https://github-readme-stats.vercel.app/api/pin/?username=AdityaInfinite&theme=dark&repo=${r.name}`;
+	let tab = "";
+	let projects = ["adityainfinite.github.io", "mastermind","schoolschedule","adidrive"];
+	// name array vals = project names on github
+	for (i = 0; i <= projects.length - 1; i++) {
+		console.log(i);
 
-                tab +=
-                    `<div class="col-sm-4.5" style="margin: 10px">
-                        <div class="card">
-                        <img src="${img}" class="card-img-top" alt="repoImage"></img>
-                        <div class="card-body">
-                            <h5 class="card-title">${r.name}</h5>
-                            <p class="card-text">${data2.description}</p>
-                            <a href="${r.html_url}" target="_blank" class="btn btn-primary">More info</a>
-                        </div>
-                        </div>
-                    </div>`;
-            }
-        }
-        $("#projectsDiv").html(tab);
-    }
+		img = `https://github-readme-stats.vercel.app/api/pin/?username=AdityaInfinite&theme=dark&repo=${projects[i]}`;
+
+		tab +=
+		`<div class="col-sm-4.5" style="margin: 10px">
+            <div class="card">
+            	<img src="${img}" class="card-img-top" alt="repoImage"></img>
+            	<div class="card-body">
+            	    <h5 class="card-title">${projects[i]}</h5>
+            	    <a href="https://www.github.com/${projects[i]}" target="_blank" class="btn btn-primary">More info</a>
+            	</div>
+            </div>
+        </div>`;
+
+	}
+	$("#projectsDiv").html(tab);
 }
